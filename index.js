c1d2c7273a07dade1a5061ee19ba20bf53ce6477
mursalin35 <msmursalin35@gmail.com>
2026-01-08 12:28:32 +0600

    all problem solved

--- index.js
+++ index.js
@@ -38,3 +38,102 @@ function isPalindrome(str) {
   }
 }
 console.log(isPalindrome("madam"));
+
+
+// Problem 4: Find the Maximum Number
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(findMax([5, 1, 9, 3]));
+
+
+// Problem 5: Remove Duplicates from an Array
+function removeDuplicates(arr) {
+  let newArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArray.includes(arr[i])) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
+
+
+// Problem 6: Sum of All Numbers in an Array
+function sumArray(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+console.log(sumArray([1, 2, 3, 4]));
+
+
+// Problem 7: Find Even Numbers in an Array
+function findEvenNumbers(arr) {
+  let evenNumbers = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNumbers.push(arr[i]);
+    }
+  }
+  return evenNumbers;
+}
+console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWords(sentence) {
+  let words = sentence.split(" ");
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word =
+      words[i][0].toUpperCase() + words[i].slice(1);
+    result.push(word);
+  }
+  return result.join(" ");
+}
+console.log(capitalizeWords("hello world"));
+
+
+// Problem 9: Find the Factorial of a Number
+function factorial(num) {
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result = result * i;
+  }
+  return result;
+}
+console.log(factorial(5));
+
+
+// Problem 10: PingPong Challenge
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+pingPong();
+
